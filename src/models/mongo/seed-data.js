8dba42ad85d84bee97c413750c6740cb8a20cd08
David Cotter <20062924@mail.wit.ie>
2023-05-20 15:00:52 +0100

    adding seeding

---
+++ src/models/mongo/seed-data.js
@@ -0,0 +1,41 @@
+export const seedData = {
+  users: {
+    _model: "User",
+    homer: {
+      firstName: "Homer",
+      lastName: "Simpson",
+      email: "homer@simpson.com",
+      password: "secret"
+    },
+    marge: {
+      firstName: "Marge",
+      lastName: "Simpson",
+      email: "marge@simpson.com",
+      password: "secret"
+    },
+    bart: {
+      firstName: "Bart",
+      lastName: "Simpson",
+      email: "bart@simpson.com",
+      password: "secret"
+    }
+  },
+  countrys: {
+    _model: "Country",
+    ireland: {
+      title: "Ireland",
+      userid: "->users.bart"
+    }
+  },
+  pointofinterests: {
+    _model: "Pointofinterest",
+    hillwalk: {
+      title: "Hillwalk",
+      county: "kerry",
+      description: "stuff happens here",
+      latitude: 2.4234,
+      longitude: -3.4564,
+      countryid: "->countrys.ireland"
+    }
+  }
+};
