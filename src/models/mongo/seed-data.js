f6b8a6c9ebef92f69f5822b86c4af200ff1b6112
David Cotter <20062924@mail.wit.ie>
2023-05-20 15:26:39 +0100

    added validation and sanisation

--- src/models/mongo/seed-data.js
+++ src/models/mongo/seed-data.js
@@ -7,19 +7,19 @@ export const seedData = {
       firstName: "Homer",
       lastName: "Simpson",
       email: "homer@simpson.com",
-      password: "secret"
+      password: "reallytopsecret"
     },
     marge: {
       firstName: "Marge",
       lastName: "Simpson",
       email: "marge@simpson.com",
-      password: "secret"
+      password: "reallytopsecret"
     },
     bart: {
       firstName: "Bart",
       lastName: "Simpson",
       email: "bart@simpson.com",
-      password: "secret"
+      password: "reallytopsecret"
     }
   },
   countrys: {
